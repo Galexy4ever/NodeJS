7b09b5f3a46d29ecdf1f5229ff9b967463b37a46
Пользователь <mctasb@yandex.ru>
2022-05-22 15:09:12 +0300

    Lesson_1_3

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 // Seive solution
-
+const colors = require('colors')
 const [ num1 ] = process.argv.slice(2)
 const [ num2 ] = process.argv.slice(3)
-
-// function getPrimes(num1, num2) {
-//     const seive = [];
-//     const primes = [];
-    
-//     for (let i = num1; i <= num2; i++) {
-//       if (!seive[i]) {
-//         primes.push(i);
-//         for (let j = i * i; j <= num2; j += i) {
-//           seive[j] = true;
-//         } 
-//       }
-//     }
-    
-    
-//     return primes;
-//   }
-  
-//   console.log(getPrimes(num1,num2));
-  
+const Colors = {GREEN : 0, YELLOW: 1, RED : 2}
+let currentColor = Colors.GREEN;
 
 
-  
-  
-//   // Check is Prime
   function isPrime(num) {
+    
     for (let i = 2, max = Math.sqrt(num); i <= max; i++) {
       if (num % i === 0) {
         return false;
@@ -36,18 +16,57 @@ const [ num2 ] = process.argv.slice(3)
     return num > 1;
   }
   
-  // Bruteforce
+ 
+  function colorDisplay() {
+    currentColor++;
+        if (currentColor > Colors.RED)
+            currentColor = Colors.GREEN;
+    }
+  
+
+function changeColor(primes) {
+
+  for (let i = 0; i <= primes.length ; i++) {
+
+    let number = primes[i]
+     
+      colorPrint(number)
+  
+      
+      
+    }
+  }
+
+  const colorPrint = (number) => {
+       
+    switch (currentColor){
+        case Colors.RED:
+            console.log(`${number}`.red);
+            break;
+        case Colors.GREEN:
+            console.log(`${number}`.green);
+            break;
+        case Colors.YELLOW:
+            console.log(`${number}`.yellow);
+            break;
+    }
+    colorDisplay();
+}
+
   function getPrimesFor(num1, num2) {
+
+    
     const primes = [];
-   
+    
     
     for (let i = num1; i <= num2 ; i++) {
+      
       if (isPrime(i)) {
         primes.push(i);
       }
     }
     
-    return primes;
+    return changeColor(primes);
   }
   
   console.log(getPrimesFor(num1, num2));
